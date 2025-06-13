f39876f6af3a6c5ffc6b4a9a4e39641161eee6fa
GreatLolz <greatlolz.676@gmail.com>
2025-06-13 19:50:10 +0000

    use ReactNode for icon type in ControlButton

--- src/components/datasets/ControlButton.tsx
+++ src/components/datasets/ControlButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@headlessui/react";
 
-export default function ControlButton({ icon, text }: { icon: any, text: string }) {
+export default function ControlButton({ icon, text }: { icon: React.ReactNode, text: string }) {
     return (
         <Button className="px-2 rounded-md flex items-center space-x-2 hover:bg-neutral-900 hover:cursor-pointer">
             {icon}
