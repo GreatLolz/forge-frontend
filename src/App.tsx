1599b9c8221bb0cfd351fe6d9ac07ce30f93f57c
GreatLolz <greatlolz.676@gmail.com>
2025-06-21 21:30:43 +0000

    implement sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router";
 import MainSidebar from "./components/MainSidebar";
-import Datasets from "./pages/Datasets";
 import Home from "./pages/Home";
 import { useEffect, useState } from "react";
 import Landing from "./pages/Landing";
 import type { UserDetails } from "./types/user";
-import User from "./components/User";
 import ApiClient from "./utils/api";
+import { SidebarTrigger } from "./components/ui/sidebar";
+import Datasets from "./pages/Datasets";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState<boolean | null>(null)
@@ -29,17 +29,17 @@ function App() {
 
   return (
     <>
-      <div className="flex h-screen bg-neutral-950 text-neutral-300">
-        {loggedIn === true ? (
+      <div className="flex h-screen bg-neutral-950 text-neutral-300 w-full">
+        {loggedIn && userDetails ? (
           <>
-            <MainSidebar />
+            <MainSidebar userDetails={userDetails}/>
+            <SidebarTrigger />
             <div className="flex-1">
               <Routes>
                 <Route index element={<Home />} />
                 <Route path="/datasets" element={<Datasets />} />
               </Routes>
             </div>
-            <User userDetails={userDetails} />
           </>
         ) : loggedIn === false ? (
           <Landing />
