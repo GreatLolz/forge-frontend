8e29acbc7124d937edb8195fd093700289ee4fff
GreatLolz <greatlolz.676@gmail.com>
2025-06-21 22:31:23 +0000

    add dataset table controls

--- src/pages/Datasets.tsx
+++ src/pages/Datasets.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import useDatasetActions from "../hooks/useDatasetActions";
 import { DataTable } from "@/components/datasets/table/DataTable";
 import { columns } from "@/components/datasets/table/columns";
+import ControlPanel from "@/components/datasets/ControlPanel";
 
 export default function Datasets() {
     const fileInputRef = useRef<HTMLInputElement>(null);
@@ -32,11 +33,31 @@ export default function Datasets() {
         
         handleImport();
     }, [importFile]);
+
+    const handleControlClick = (action: string) => {
+        switch (action) {
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "refresh":
+                break;
+            case "import":
+                break;
+            case "export":
+                break;
+            case "delete":
+                break;
+        }
+    }
     
     return (
         <div className="p-10 h-screen flex flex-col">
             <input className="hidden" type="file" onChange={(e) => setImportFile(e.target.files?.[0] || null)} ref={fileInputRef} />
             <h1 className="text-2xl font-bold mb-10">Datasets</h1>
+            <div className="mb-2">
+                <ControlPanel handleControlClick={handleControlClick}/>
+            </div>
             <DataTable columns={columns} data={datasets}/>
         </div>
     )
