98cd5d330459e555755e4e7d2eef03aeb20408b9
GreatLolz <greatlolz.676@gmail.com>
2025-06-23 16:40:14 +0000

    improve displaying cells in dataset table

--- src/components/datasets/table/columns.tsx
+++ src/components/datasets/table/columns.tsx
@@ -1,5 +1,5 @@
 import { Checkbox } from "@/components/ui/checkbox";
-import type { Dataset } from "@/types/datasets";
+import { DATASET_TYPES, type Dataset } from "@/types/datasets";
 import { type ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<Dataset>[] = [
@@ -36,17 +36,21 @@ export const columns: ColumnDef<Dataset>[] = [
     {
         accessorKey: "type",
         header: "Type",
+        cell: ({row}) => DATASET_TYPES[row.original.type] || row.original.type
     },
     {
         accessorKey: "created_at",
         header: "Created at",
+        cell: ({row}) => new Date(row.original.created_at).toLocaleString()
     },
     {
         accessorKey: "updated_at",
         header: "Updated at",
+        cell: ({row}) => new Date(row.original.updated_at).toLocaleString()
     },
     {
         accessorKey: "sample_count",
         header: "Sample count",
+        cell: ({row}) => row.original.sample_count === 0 ? "-" : row.original.sample_count
     },
 ]
